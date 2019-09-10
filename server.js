bb3bc5a5174c8195531d602275057fdc07016577
Jarrod Krasnoff <Jarrodk0506@outlook.com>
2019-09-10 14:41:42 -0400

    make paths for displaying home and the switch the the survey on button click

--- server.js
+++ server.js
@@ -7,3 +7,17 @@ var PORT = 3500;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "app/public/home.html"));
+});
+
+app.get("/survey", function (req, res) {
+    res.sendFile(path.join(__dirname, "app/public/survey.html"));
+});
+
+app.listen(PORT, function () {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+});
