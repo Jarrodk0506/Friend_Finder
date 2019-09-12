f7d3f603336f5073e573abb67851aeb56955ea4d
Jarrod Krasnoff <Jarrodk0506@outlook.com>
2019-09-11 21:54:48 -0400

    Finishing touches on modal and funciontality

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,2 +1,37 @@
+require("../routing/apiRoutes");
 
-var friends =[];
+
+var friends =[{
+    name: "Jarrod",
+    link: "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png",
+    scores: [5,4,3,2,1,5,4,3,2,1]
+},
+{
+    name: "Ryan",
+    link: "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png",
+    scores: [3,2,5,2,3,5,3,4,4,1]
+},
+{
+    name: "Mik",
+    link: "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png",
+    scores: [4,2,5,3,2,2,5,3,2,1]
+},
+{
+    name: "Greg",
+    link: "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png",
+    scores: [5,3,2,4,2,4,5,3,3,4]
+},
+{
+    name: "Evelin",
+    link: "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png",
+    scores: [4,3,5,3,2,1,1,1,3,5]
+},
+{
+    name: "Tim",
+    link: "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png",
+    scores: [2,3,1,3,2,2,4,5,3,5]
+},
+];
+
+
+module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,14 +1,16 @@
+var friends = require("../data/friends");
+
+
 module.exports = function(app){
 
-app.post("/api/friends", function(req, res){
-    var newfriend = req.body;
-    friends.push(newfriend);
-    res.json(newfriend);
-    console.log("New friend added!");
-});
+    app.get("/api/friends", function (req, res) {
+        res.json(friends);
+    })
 
-app.get("/api/friends", function(req, res){
-    res.json(friends);
-})
-
+    app.post("/api/friends", function (req, res) {
+        var newfriend = req.body;
+        friends.push(newfriend);
+        res.json(newfriend);
+        console.log("New friend added!");
+    });
 }
